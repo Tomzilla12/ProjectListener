3b21b047ae445eaefc07f6d45d7202385b6e8168
Unknown <tom.diggelmann@swisscom.com>
2017-09-16 01:37:14 +0200

    List all subdirectories except for 'assets'

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Projects</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<link rel="stylesheet" href="assets/css/main.css" />
+  </head>
+  <body>
+    <div class="main">
+      <?php
+      $dirs = array_filter(glob('*'), 'is_dir');
+      $pattern = '/^assets$/';
+      foreach($dirs as $dir) {
+        if (!preg_match($pattern, $dir)) {
+          print("<a href=\"$dir\">
+          <div class=\"project\">
+            <h4 class=\"text\">$dir</h4>
+          </div>
+        </a>");
+        }
+      }
+      ?>
+
+    <!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/main.js"></script>
+  </body>
+</html>
